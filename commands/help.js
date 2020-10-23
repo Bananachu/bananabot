d9f319d2054436341ea63786f82a4581db4aeec2
Bananachu <62444046+Bananachu@users.noreply.github.com>
2020-10-23 11:14:33 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,20 @@
 const Command = require('./command')
+const Discord = require('discord.js');
 
+const embed = new Discord.MessageEmbed()
+	.setColor('#7289da')
+	.setTitle('Liste des commandes')
+	.setDescription('Voici la liste des commandes disponibles pour le Bananabot.')
+	.setThumbnail('https://i.imgur.com/NhvMsWx.png')
+	.addFields(
+		{ name: '\ud83d\udd0e Commandes de recherche', value: '``*google [mot clef]`` : Recherche le mot clef sur Google\n``*wpedia [mot clef]`` : Recherche le mot clef sur Wikipédia' },
+		{ name: '\ud83c\udfb6 Commandes musicales', value: '``*play [lien YouTube]`` : Joue la vidéo dans un salon vocal\n``*stop`` : Arrête la lecture de la vidéo en cours' },
+		{ name: '\ud83c\udf4c Commandes autres', value: '``*ping`` et ``*pong`` : Pour jouer au ping-pong\n``*pokemon`` : Pour capturer un Pokémon au hasard' },
+		{ name: '\ud83d\uded1 Commandes administratives', value: '``*delete [x]`` : Supprime ``x`` messages' },
+	)
+	.setFooter('Ver. 0.4.2.1 (bêta)');
+	
+	
 module.exports = class Help extends Command {
 
 
@@ -9,7 +24,7 @@ module.exports = class Help extends Command {
 
 
 	static action (message) {
-		message.channel.send('Voici la liste des commandes par cat\u00e9gories :\n\n\ud83d\udd0e __Commandes de recherche :__\n``*google [mot clef]`` : Recherche le mot clef sur Google\n``*wpedia [mot clef]`` : Recherche le mot clef sur Wikip\u00e9dia\n\n\ud83c\udfb6 __Commandes musicales : __\n``*play [lien YouTube]`` : Joue la vid\u00e9o dans un salon vocal\n``*stop`` : Arr\u00eate la lecture de la vid\u00e9o en cours\n\n\ud83c\udf4c __Commandes autres__ :\n``*ping`` et ``*pong`` : Pour jouer au ping-pong\n``*pokemon`` : Pour capturer un Pok\u00e9mon au hasard (b\u00eata)\n\nVer. 0.4.4 (b\u00eata). Si vous rencontrez un probl\u00e8me, contactez Bananachu')
+		message.channel.send(embed)
 	}
 
 
