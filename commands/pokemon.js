3c117aa2aa727654d9dfff24d4237a4be9790985
Bananachu <62444046+Bananachu@users.noreply.github.com>
2022-06-17 22:44:52 +0200

    optimisation + 12 more Pokémon + minor changes
    see index.js

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -1,38 +1,19 @@
-const Command = require('./command')
 const Discord = require('discord.js')
 
+module.exports = class Pokemon {
+	static action (message) {	
+	var poke = Math.floor(Math.random() * 905) + 1
+	if (poke < 100) poke = ("0" + (poke));
+	if (poke < 10) poke = ("0" + (poke));
 
-
-module.exports = class Pokemon extends Command {
-
-
-	static match (message) {
-		return message.content.startsWith('*pokemon')
-	}
-
-
-	static action (message) {
-		
-	var poke = Math.floor(Math.random() * (893 - 1 +1)) + 1
-	
-		if (poke < 100) {
-	poke = ("0" + (poke))
-}
-
-		if (poke < 10) {
-	poke = ("0" + (poke))
-}
-
-const embed = new Discord.MessageEmbed()
-	.setColor('#3498db')
+	var embed = new Discord.MessageEmbed()
+	.setColor('#5865f2')
 	.setTitle('Voir sa fiche Pokédex ?')
 	.setURL(('https://www.pokepedia.fr/Pok%C3%A9mon_n%C2%B0' + poke))
-	.setAuthor(`${message.author.tag}, a capturé ce Pokémon !`, 'https://www.pokepedia.fr/images/8/87/Pok%C3%A9_Ball.png')
+	.setAuthor(`${message.author.tag} a capturé ce Pokémon !`, 'https://www.pokepedia.fr/images/8/87/Pok%C3%A9_Ball.png')
 	.setImage('https://www.serebii.net/pokemon/art/' + poke + '.png')
-	.setTimestamp()
+	.setTimestamp();
 
-	message.channel.send(embed)
+	message.channel.send(embed);
 	}
-
-
 }
